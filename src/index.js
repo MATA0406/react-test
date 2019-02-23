4c03114ea343f211d1adc80fecda7d04ddad0fdd
MATA0406 <jinho_46@naver.com>
2019-02-23 15:11:47 +0900

    aaaa

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { area, circumference } from './js/circle';
+import box from './js/cube'
+
+function component(){
+  let element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+console.log(area(5), circumference(10))
+console.log(box(10))
